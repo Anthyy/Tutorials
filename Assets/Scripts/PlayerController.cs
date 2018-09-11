874790350f728156b114511d4276a87c11134794
Anthyy <42423921+Anthyy@users.noreply.github.com>
2018-09-11 15:44:55 +1000

    Initial Commit
    Using NavMeshComponents Plugin (allows you to rotate NavMeshes)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI; // Without these namespaces, you can't access certain API
+
+/*
+ * The PlayerController handles player movement & rotation logic
+ */
+
+public class PlayerController : MonoBehaviour
+{
+    public NavMeshAgent agent; // reference to NavMeshAgent component
+
+    private Vector3 targetPos;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If the targetPos is valid (is not zero)
+        if(targetPos.magnitude > 0)
+        {
+            // Follow the taret!
+            agent.SetDestination(targetPos);
+        }
+    }
+
+    // Call this to tell agent where to go!
+    public void SetTarget(Vector3 target)
+    {
+        // Set the new target
+        targetPos = target;
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ *  The PlayerInput is in charge of receiving input from the user and interacting with PlayerController
+ */
+public class PlayerInput : MonoBehaviour
+{
+    public PlayerController controller;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if Mouse Button is down
+        // 0 = Left
+        // 1 = Right
+        // 2 = Middle
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Get ray from camera
+            Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            // Raycast out to hit something
+            if (Physics.Raycast(camRay, out hit, 1000f))
+            {
+                // Update the target with the thing we hit (the point)
+                controller.SetTarget(hit.point);
+            }
+        }
+
+
+        
+    }
+}
